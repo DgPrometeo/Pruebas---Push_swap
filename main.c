8c9312f0000f3614d2c49c894aafa9a477d799c7
Daniel García <danielgarciasan93@gmail.com>
2023-09-16 20:07:14 +0200

    Now I will start with list

--- main.c
+++ main.c
@@ -6,18 +6,28 @@
 /*   By: danielga <danielga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 17:09:22 by danielga          #+#    #+#             */
-/*   Updated: 2023/06/08 19:23:56 by danielga         ###   ########.fr       */
+/*   Updated: 2023/09/16 19:20:11 by danielga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	main(int argc, char **argv)
+static void	ft_free_all(t_stack info)
 {
-	char	**stack_argv;
+	
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack	*data;
 	int		i;
 
-	if (argc < 2)
+	data = (t_stack *)malloc(sizeof(t_stack));
+	if (!data)
+		exit(0);
+	if (argc == 2)
+		data->total_size = 
+	/*if (argc < 2)
 		return (0);
 	if (argc == 2)
 		stack_argv = ft_split(argv[1], 32);
@@ -29,9 +39,10 @@ void	main(int argc, char **argv)
 			return (NULL);
 		while (i < (argc - 1))
 		{
-			stack_argv[i] = ft_strdup(argv[1+i]);
+			stack_argv[i] = ft_strdup(argv[1 + i]);
 			i++;
 		}
+		stack_argv[i] = NULL;
 	}
 	i = 0;
 	while (stack_argv[i] != '\0')
@@ -39,7 +50,7 @@ void	main(int argc, char **argv)
 		if (ft_isdigit(stack_argv[i]) == 0)
 			return (0);
 		i++;
-	}
+	}*/
 	return (0);
 }
 
@@ -55,3 +66,8 @@ si son más de dos argumentos, tenemos que establecer un malloc del tamaño de a
 y enviarlo a un strdup para que vaya metiendo cada argumento en una celda del
 mismo.
  */
+/*
+La el programa push_swap debe de contener:
+- Creación de pilas A y B.
+- 
+*/
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: danielga <danielga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 17:10:30 by danielga          #+#    #+#             */
-/*   Updated: 2023/06/07 15:10:40 by danielga         ###   ########.fr       */
+/*   Updated: 2023/09/16 19:12:00 by danielga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,32 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include "./libft/libft.h"
-# include "./ft_printf/ft_printf.h"
 
+/**
+ * @brief Estructura básica con la información de los stacks.
+ * 
+ * @param stack_a Puntero al Stack A.
+ * @param stack_b Puntero al Stack B.
+ * @param size_a Tamaño del Stack A.
+ * @param size_b Tamaño del Stack B.
+ * @param total_size Tamaño total del Stack.
+ * 
+*/
+typedef struct s_stack
+{
+	int		*stack_a;
+	int		*stack_b;
+	int		size_a;
+	int		size_b;
+//	char	*str;
+//	int		max;
+	int		total_size;
+}			t_stack;
 
-void	push_swap(int argc, char **argv);
+int			main(int argc, char **argv);
+int			ft_check_dig(char **str);
+void		ft_error(void);
+static void	ft_free_all(t_stack info);
 
 #endif
 
